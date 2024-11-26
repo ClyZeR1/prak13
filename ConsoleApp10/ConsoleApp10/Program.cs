25a64838f81af9ea6c83220099f5ca8c9a2c4158
ClyZeR1 <sergeistarodyb@gmail.com>
2024-11-26 19:01:29 +0300

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static double Average(int m, int n)
+        {
+            int sum = 0;
+            int count = 0;
+            for (int i = m; i <= n; i++)
+            {
+                sum += i;
+                count++;
+            }
+            return (double)sum / count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(1, 10));
+            Console.WriteLine(Average(20, 40));
+            Console.Write("a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Average(a, b));
+
+            Console.ReadLine();
+        }
+    }
+}
