c2e35899313e1e304f85e0e51f38502ad5582ffe
ClyZeR1 <sergeistarodyb@gmail.com>
2024-11-26 14:07:30 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Min(int a, int b, int c)
+        {
+            int min = a;
+            if (b < min) min = b;
+            if (c < min) min = c;
+            return min;
+        }
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void Output(int result)
+        {
+            Console.WriteLine("Минимальное значение: " + result);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = Input("a = ");
+            int b = Input("b = ");
+            int c = Input("c = ");
+
+            int min = Min(a, b, c);
+
+            Output(min);
+
+            Console.ReadLine();
+        }
+    }
+}
