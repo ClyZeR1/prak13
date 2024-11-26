dbf8ae3389f6276cf521650b8eab6c3149724cbb
ClyZeR1 <sergeistarodyb@gmail.com>
2024-11-26 18:58:36 +0300

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Numbers(int n)
+        {
+            for (int i = 2; i <= n; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Numbers(20);
+            Numbers(50);
+
+            Console.Write("m:");
+            int m = int.Parse(Console.ReadLine());
+            Numbers(m);
+
+            Console.ReadLine();
+        }
+    }
+}
